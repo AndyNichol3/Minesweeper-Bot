82bae749578d91871df68898b969a0429a318d44
AndyNichol3 <125906484+AndyNichol3@users.noreply.github.com>
2023-12-31 19:44:30 +0000

    the addition of the first move randomly generated by the bot

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 // minesweeper
 
 // g++ main.cpp PLAY_GAME.cpp
-
 #include "PLAY_GAME.h"
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -53,13 +53,39 @@ int main() {
                                 vector<int>(maxNumberOfColumns, 0));
   vector<vector<bool>> boolGameBoard(maxNumberOfRows,
                                      vector<bool>(maxNumberOfColumns, false));
+  vector<vector<bool>> boolFlagLocation(maxNumberOfRows,
+                                        vector<bool>(maxNumberOfColumns, false));
+  
 
   // handles all of the creation, and the round 1 of them game
   // since the game cannot be lost on round one and the variables
   // are not reused anywhere else
-  initalizeGameBoard(boolGameBoard, gameBoard, maxNumberOfRows,
-                     maxNumberOfColumns, maxNumOfMines);
+  
+ // initalizeGameBoard(boolGameBoard, gameBoard, maxNumberOfRows,
+        //             maxNumberOfColumns, maxNumOfMines);
+//{
+  printBoolBoard(boolGameBoard, gameBoard, maxNumberOfRows, maxNumberOfColumns);
+  printRoundHeader(1);
 
+  //cout << "Enter Starting Point (-1 to exit): " << endl;
+  cout << "the bot will now start" << endl;
+  srand(time(NULL));
+  int userStartCol = rand() % (maxNumberOfColumns - 1);
+  cout << "bot chose X: " << userStartCol << endl;
+  int userStartRow = rand() % (maxNumberOfRows - 1);
+  cout << "bot chose Y: " << userStartRow << endl;
+
+  cout << endl;
+
+  boolGameBoard[userStartRow][userStartCol] = true;
+
+  fillWithMines(gameBoard, userStartRow, userStartCol, maxNumOfMines,
+                maxNumberOfRows, maxNumberOfColumns);
+  fillWithInts(gameBoard, maxNumberOfRows, maxNumberOfColumns);
+  recursiveRevealExplosion(gameBoard, boolGameBoard, userStartRow, userStartCol,
+                           maxNumberOfRows, maxNumberOfColumns);
+  printBoolBoard(boolGameBoard, gameBoard, maxNumberOfRows, maxNumberOfColumns);
+// }
   bool win = playGame(maxNumberOfColumns, maxNumberOfRows, boolGameBoard,
                       gameBoard, maxNumOfMines);
 
